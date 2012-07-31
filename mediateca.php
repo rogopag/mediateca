0b4d65692a7938d959e5a9c17254abe2646bfb06
Riccardo Strobbia <riccardo@robanostra.com>
2012-07-31 16:46:53 +0200

    Besic Structure Files

---
+++ mediateca.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Mediateca
+Plugin URI: http://dito.areato.org
+Description: displays and manages madiateca content
+Version: 0.1
+Author: Riccardo, Paolo, Stefano
+Author URI: http://foo.bar
+License: A "Slug" license name e.g. GPL2
+*/
+?>
